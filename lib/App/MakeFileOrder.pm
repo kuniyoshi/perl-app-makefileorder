be25d8991a285bcf80cd662e0aa8372326fa3fbc
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2011-01-15 22:38:26 +0900

    INIT.

---
+++ lib/App/MakeFileOrder.pm
@@ -0,0 +1,44 @@
+package App::MakeFileOrder;
+
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+1;
+__END__
+=encoding utf-8
+
+=head1 NAME
+
+App::MakeFileOrder - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use App::MakeFileOrder;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Blah blah blah.
+
+=head1 SEE ALSO
+
+Blah blah blah.
+
+=head1 AUTHOR
+
+kuniyoshi kouji, E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 by kuniyoshi kouji
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.12.1 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
+
